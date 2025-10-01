e8516e7eb3cd9e94e4c797101a99c86a2215fcff
SamInTheShell <sam@hyperkube.org>
2025-10-01 02:48:11 -0500

    autocompletions and snippets

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -16,5 +16,12 @@ return {
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+		-- Auto-format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 	end,
 }
