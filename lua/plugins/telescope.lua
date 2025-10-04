dea5473c8dae24ed9db811689f761c291299b01e
SamInTheShell <sam@hyperkube.org>
2025-10-03 23:42:46 -0500

    hack in grep with post filter search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,9 +12,51 @@ return {
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Telescope commands" })
 		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
-		vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Telescope keymaps" })
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope keymaps" })
+		vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Telescope marks" })
+		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope oldfiles (recent files)" })
 		vim.keymap.set("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
 		vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+
+		-- Add custom grep subcommand after telescope loads
+		vim.defer_fn(function()
+			-- Delete existing telescope command
+			vim.cmd("delcommand Telescope")
+
+			-- Recreate with our custom logic
+			vim.api.nvim_create_user_command("Telescope", function(opts)
+				local subcommand = opts.fargs[1]
+
+				if subcommand == "grep" then
+					local search_term = table.concat(opts.fargs, " ", 2)
+					if search_term == "" then
+						builtin.grep_string({ search = vim.fn.input("Grep > ") })
+					else
+						builtin.grep_string({ search = search_term })
+					end
+				else
+					-- Use telescope's internal command handling
+					require("telescope.command").load_command(unpack(opts.fargs))
+				end
+			end, {
+				nargs = "*",
+				complete = function(ArgLead, CmdLine, CursorPos)
+					local completions = {}
+
+					-- Get from telescope's builtin functions
+					for name, _ in pairs(builtin) do
+						if name:match("^" .. ArgLead) then
+							table.insert(completions, name)
+						end
+					end
+
+					-- Add our custom grep command
+					if string.match("grep", "^" .. ArgLead) then
+						table.insert(completions, "grep")
+					end
+
+					return completions
+				end,
+			})
+		end, 100)
 	end,
 }
