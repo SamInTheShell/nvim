a739ec3b66e37db5e7440bacc7e4fab3f67af806
SamInTheShell <sam@hyperkube.org>
2025-10-08 18:57:07 -0500

    If only nvim-tree is open, close vim

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -36,6 +36,18 @@ return {
 			},
 		})
 
+		-- Auto-close vim when only nvim-tree is open
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+			pattern = "NvimTree_*",
+			callback = function()
+				local layout = vim.api.nvim_call_function("winlayout", {})
+				if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then
+					vim.cmd("confirm quit")
+				end
+			end
+		})
+
 		vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>")
 	end,
 }
