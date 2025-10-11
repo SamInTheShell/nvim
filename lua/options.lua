d525b79c06cb6bbbf6c21b8ec8a031022845b4d3
SamInTheShell <sam@hyperkube.org>
2025-10-11 17:36:03 -0500

    purge comments for tabs

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,4 @@
 vim.cmd("set expandtab")
--- vim.cmd("set tabstop=2")
--- vim.cmd("set softtabstop=2")
--- vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.opt.numberwidth = 4
 vim.opt.signcolumn = "yes:2"
