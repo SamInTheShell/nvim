22597f1283a0b1c93708f8695b8425a896936d92
SamInTheShell <sam@hyperkube.org>
2025-10-11 22:10:05 -0500

    update lualine to show full filepaths (prefixed with ~ if in homedir)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,14 @@ return {
 	config = function()
 		require("lualine").setup({
 			theme = "dracula",
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 3, -- 0 = just filename, 1 = relative path, 2 = absolute path, 3 = absolute path with ~ for home
+					},
+				},
+			},
 		})
 	end,
 }
