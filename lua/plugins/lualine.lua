654f2623bbab254286d493b6a20c41f449b995ed
SamInTheShell <sam@hyperkube.org>
2025-10-17 01:29:33 -0500

    Selection support indicator

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,6 +11,17 @@ return {
 						path = 3, -- 0 = just filename, 1 = relative path, 2 = absolute path, 3 = absolute path with ~ for home
 					},
 				},
+				lualine_x = {
+					{
+						function()
+							return vim.o.mouse == "a" and "" or ""
+						end,
+						color = { fg = "#50fa7b" }, -- Green color
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
 			},
 		})
 	end,
