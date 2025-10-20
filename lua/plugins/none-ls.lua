8198e753b3209c888833a7db48b893ff86535402
SamInTheShell <sam@hyperkube.org>
2025-10-20 14:47:38 -0500

    disable json formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -19,10 +19,12 @@ return {
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
-		-- Auto-format on save
+		-- Auto-format on save (excluding JSON files)
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			callback = function()
-				vim.lsp.buf.format({ async = false })
+				if vim.bo.filetype ~= "json" then
+					vim.lsp.buf.format({ async = false })
+				end
 			end,
 		})
 	end,
