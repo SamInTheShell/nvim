29675e8d4f51f3811fccb16e991dc9a63d764f5e
SamInTheShell <sam@hyperkube.org>
2025-10-21 03:38:59 -0500

    Toggle which-key with <leader>tw

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,12 +1,42 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	opts = {
-		preset = "modern",
-		win = {
-			border = "rounded",
-		},
-		spec = {
+	config = function()
+		local wk = require("which-key")
+		
+		-- Store the original delay
+		local enabled_delay = 500
+		local disabled_delay = 999999
+		local current_delay = enabled_delay
+		
+		wk.setup({
+			preset = "helix",
+			delay = function() return current_delay end,
+			win = {
+				border = "rounded",
+				padding = { 1, 2 },
+				wo = {
+					winblend = 0,
+				},
+			},
+		})
+
+		-- Toggle function
+		local function toggle_which_key()
+			if current_delay == enabled_delay then
+				current_delay = disabled_delay
+				print("which-key disabled")
+			else
+				current_delay = enabled_delay
+				print("which-key enabled")
+			end
+		end
+
+		-- Toggle keybinding
+		vim.keymap.set("n", "<leader>tw", toggle_which_key, { desc = "Toggle which-key" })
+
+		-- Add groups
+		wk.add({
 			{ "<leader>f", group = "Find/File" },
 			{ "<leader>g", group = "Git" },
 			{ "<leader>l", group = "LSP" },
@@ -14,6 +44,6 @@ return {
 			{ "<leader>t", group = "Terminal/Toggle" },
 			{ "<leader>w", group = "Window" },
 			{ "<leader>b", group = "Buffer" },
-		},
-	},
+		})
+	end,
 }
